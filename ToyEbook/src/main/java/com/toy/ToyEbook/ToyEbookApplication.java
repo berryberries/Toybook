694b522e95bd84ae2cc27c998a8222df2c219366
berryberries <yun1599@naver.com>
2023-11-13 23:14:25 +0900

    11.13 설정

---
+++ ToyEbook/src/main/java/com/toy/ToyEbook/ToyEbookApplication.java
@@ -0,0 +1,13 @@
+package com.toy.ToyEbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyEbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyEbookApplication.class, args);
+	}
+
+}
---
+++ ToyEbook/src/test/java/com/toy/ToyEbook/ToyEbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.ToyEbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyEbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
