6412d29a8c4dbe1b74693105ea5db5ae5327e0e1
berryberries <yun1599@naver.com>
2023-12-04 23:06:38 +0900

    12.04

--- ToyEbook/src/main/java/com/toy/ToyEbook/member/service/impl/MemberServiceImple.java
+++ ToyEbook/src/main/java/com/toy/ToyEbook/member/service/impl/MemberServiceImple.java
@@ -2,7 +2,16 @@ package com.toy.ToyEbook.member.service.impl;
 
 import org.springframework.stereotype.Service;
 
+import com.toy.ToyEbook.member.dto.Member;
+import com.toy.ToyEbook.member.service.face.MemberService;
+
 @Service
-public class MemberServiceImple {
+public class MemberServiceImple implements MemberService {
+
+	@Override
+	public Member getUserInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
